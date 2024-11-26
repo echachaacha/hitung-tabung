32db2add8e548b7af229744f6eee8729b69ebe35
echachaacha <hasnahumaira1905@gmail.com>
2024-11-26 17:11:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ t = st.number_input("Masukkan Tinggi (cm): ",0)
 if st.button("Hitung Volume", type="primary"):
   loading = st.progress(0)
   for i in range(100):
-    time,sleep(0.01)
+    time.sleep(0.01)
     loading.progress(i+1)
     
   v = math.pi*(r**2)*t
